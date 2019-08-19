f80821c7f9c740c1c3260ff8881dda671b7d2df1
Rexben <rexben.rb@gmail.com>
2019-08-19 13:16:45 +0100

    ch: setup NODE_ENV

--- start.js
+++ start.js
@@ -2,4 +2,7 @@ const app = require('./index');
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, console.log('App running on', PORT));
+app.listen(
+  PORT,
+  console.log(`App running on ${PORT}, in ${process.env.NODE_ENV}`),
+);
