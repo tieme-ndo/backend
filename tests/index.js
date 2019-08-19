b7d2a64424ddbb0d546ca98557e454b995de4546
Rexben <rexben.rb@gmail.com>
2019-08-19 17:56:10 +0100

    add unit tests

---
+++ tests/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../index');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('GET /', () => {
+  it('it should get homepage', done => {
+    chai
+      .request(server)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done(err);
+      });
+  });
+});
+
+describe('Invalid route', () => {
+  it('It should return invalid route', done => {
+    chai
+      .request(server)
+      .get('/auth/signup')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done(err);
+      });
+  });
+});
