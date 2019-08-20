b5622b874b175b83c185f94a79d1e7249939439d
Rexben <rexben.rb@gmail.com>
2019-08-20 09:57:15 +0100

    update schema name

--- models/schema/userSchema.js
+++ models/schema/userSchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
 
-const schema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -17,4 +17,4 @@ const schema = mongoose.Schema({
   date: { type: Date, default: Date.now },
 });
 
-module.exports = schema;
+module.exports = userSchema;
