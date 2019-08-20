7b7a2c882cdc58dd6e4e0d969f08787e39e80f17
Rexben <rexben.rb@gmail.com>
2019-08-20 09:46:20 +0100

    update schema name

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const schema = require('./schema/userSchema');
+const userSchema = require('./schema/userSchema');
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, (err) => {
   if (err) return err;
   return console.log('Successfully connected');
 });
 
-const User = mongoose.model('Users', schema);
+const User = mongoose.model('Users', userSchema);
 
 module.exports = { User };
