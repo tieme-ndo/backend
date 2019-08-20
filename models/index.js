9d0203b76d208bd259230a166fd9bbc276a01d5a
Rexben <rexben.rb@gmail.com>
2019-08-20 08:59:34 +0100

    ft: create users schema

---
+++ models/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const schema = require('./schema/userSchema');
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, (err) => {
+  if (err) return err;
+  return console.log('Successfully connected');
+});
+
+const User = mongoose.model('Users', schema);
+
+module.exports = { User };
