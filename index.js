d523e2dc2ebbb2d649e8db255e313361c648e516
Sulaiman Abidemi <andela@Andelas-MacBook-Pro-5.local>
2019-08-20 17:23:28 +0100

    ch(error handler): create error helper function
     - create resource not found error handler
     - create bad request error handler
     - create resource conflict error handler
     - create server error handler
     - create invalid authentication error handler

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const allErrorHandler = require('./middleware/errors');
+const { NOT_FOUND } = require('./helpers/error');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(logger('dev'));
+app.use(helmet());
 
-app.get('/', (req, res) => {
-  res.status(200).json('Server is live');
-});
+app.get('/', (req, res) => res.status(200).json({
+  success: true,
+  message: 'API is alive...',
+}));
 
-app.use('*', (req, res) => {
-  res.status(404).json('Invalid route');
-});
+// Handle invalid request
+app.all('*', (req, res) => res.status(NOT_FOUND).json({
+  success: false,
+  message: 'Route does not exist...'
+}));
+
+app.use(allErrorHandler());
 
 module.exports = app;
