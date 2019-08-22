8356cf6d917c59c14fb74ef9eeb6cf4d8d5292b9
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-22 11:16:29 +0100

    update: imported farmer route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const user = require('./user');
+const farmer = require('./farmer');
 
 router.use(user);
+router.use(farmer);
 
 module.exports = router;
