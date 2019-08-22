77eca7e21821bced5948b044f22c0db24a571aed
abidex4yemi <abidex4yemi@gmail.com>
2019-08-22 11:21:47 +0100

    Create isAuthorized function to validate if user is admin

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/user');
 const validate = require('../middlewares/Validations/user');
+const verifyToken = require('../middlewares/verifyToken');
 
 const userRouter = express.Router();
 
-userRouter.post('/user', validate.createUser, userController.register);
+userRouter.post('/user', verifyToken, validate.createUser, userController.register);
 
 module.exports = userRouter;
