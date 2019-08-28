8745178284286b37293c0045779d3a094ee787ea
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-28 22:13:46 +0100

    created new endpoints

--- routes/farmerRouter.js
+++ routes/farmerRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const farmerController = require('../controllers/farmer');
+const FarmerController = require('../controllers/farmer');
 const verifyToken = require('../middlewares/verifyToken');
 const validate = require('../middlewares/Validations/farmer');
 
@@ -9,17 +9,24 @@ farmerRouter.post(
   '/farmers/create',
   verifyToken,
   validate.createFarmer,
-  farmerController.addFarmer
+  FarmerController.addFarmer
 );
 
-farmerRouter.get('/farmers', verifyToken, farmerController.getAllFarmers);
-farmerRouter.get('/farmers/:id', verifyToken, farmerController.getFarmerById);
+farmerRouter.get('/farmers', verifyToken, FarmerController.getAllFarmers);
+farmerRouter.get('/farmers/:id', verifyToken, validate.validateId, FarmerController.getFarmerById);
 farmerRouter.put(
   '/farmers/:id/update',
   verifyToken,
   validate.validateId,
   validate.createFarmer,
-  farmerController.updateFarmer
+  FarmerController.updateFarmer
+);
+
+farmerRouter.delete(
+  '/farmers/:id/delete',
+  verifyToken,
+  validate.validateId,
+  FarmerController.deleteFarmer
 );
 
 module.exports = farmerRouter;
