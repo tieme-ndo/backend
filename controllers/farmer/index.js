22611675c644f5e80645861c9894061ed12ca28c
Rexben <rexben.rb@gmail.com>
2019-08-28 12:31:28 +0100

    create delete farmer controller

--- controllers/farmer/index.js
+++ controllers/farmer/index.js
@@ -1,4 +1,5 @@
 const addFarmer = require('./addFarmer');
 const updateFarmer = require('./updateFarmer');
+const deleteFarmer = require('./deleteFarmer');
 
-module.exports = { addFarmer, updateFarmer };
+module.exports = { addFarmer, updateFarmer, deleteFarmer };
