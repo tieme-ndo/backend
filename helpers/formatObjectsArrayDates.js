84413e5799550f1a3545b0365c1be574899b3210
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-09-11 11:05:21 +0100

    Add Comments & Todos + Rename Variable

--- helpers/formatObjectsArrayDates.js
+++ helpers/formatObjectsArrayDates.js
@@ -12,20 +12,22 @@ const formatObjectsArrayDates = (propertiesPath = [], ObjectsArray) => {
     throw new Error("Make sure you pass in an array of objects");
   }
 
-  const objectIsArray =
+  const ObjectsArrayIsArray =
     Object.prototype.toString.call(ObjectsArray) === "[object Array]";
 
-  if (!objectIsArray) {
+  if (!ObjectsArrayIsArray) {
     throw new TypeError("Array of objects required as a parameter");
   }
 
   const objectsArrayCopy = JSON.parse(JSON.stringify(ObjectsArray));
 
   for (const prop in propertiesPath) {
-    // takes the string property path and splits each of them into an array of properties
+    // Takes the string property path and splits each of them into an array of properties
     // i.e. ['prop.nested_prop'] => ['prop', 'nested_prop']
     const propertiesArray = propertiesPath[prop].split(".");
     objectsArrayCopy.map(object => {
+      // Currently this solution only works with a nested object 2 levels deep
+      // TODO: Allow objects with properties nested n deep
       propertiesArray.reduce((firstProperty, nestedProperty) => {
         if (!!(firstProperty && nestedProperty)) {
           object[firstProperty][nestedProperty] = object[firstProperty][
