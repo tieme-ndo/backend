eb0fe33672a418422b440b961552767791e3a373
Pav0l <pavol.karas@gmail.com>
2019-09-11 21:48:16 +0200

    Connect exception monitor

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const Sentry = require('@sentry/node');
 const allErrorHandler = require('./middlewares/errors');
 const { NOT_FOUND } = require('./helpers/error');
 const router = require('./routes');
 const { connectDB } = require('./models');
 
 const app = express();
+
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn: 'https://0636ca6c9d0845f498d09b83b1303970@sentry.io/1724713',
+    environment: process.env.NODE_ENV
+  });
+}
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
 
 app.use(express.json());
 app.use(cors());
@@ -17,17 +28,20 @@ app.use(helmet());
 connectDB();
 
 app.get('/', (req, res) => res.status(200).json({
-  success: true,
-  message: 'API is alive...',
-}));
+    success: true,
+    message: 'API is alive...'
+  }));
 
 app.use('/api/v1', router);
 
 // Handle invalid request
 app.all('*', (req, res) => res.status(NOT_FOUND).json({
-  success: false,
-  message: 'Route does not exist...'
-}));
+    success: false,
+    message: 'Route does not exist...'
+  }));
+
+// The Sentry error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(allErrorHandler());
 
