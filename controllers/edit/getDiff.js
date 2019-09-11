d96ff0b6e2ed63a6da274980f61469649368a5ea
David Quiñones <daquinons@users.noreply.github.com>
2019-09-11 16:09:36 +0200

    Improve array cases in getDiff

--- controllers/edit/getDiff.js
+++ controllers/edit/getDiff.js
@@ -3,23 +3,42 @@ const { models } = require('../../models');
 
 const getDiff = async edit => {
   const originalFarmerData = await models.Farmer.findById(edit.farmer_id);
+  const farmerObject = originalFarmerData.toObject();
   const originalFarmer = {
-    personalInfo: originalFarmerData.personalInfo,
-    familyInfo: originalFarmerData.familyInfo,
-    farmInfo: originalFarmerData.farmInfo,
-    guarantor: originalFarmerData.guarantor
+    personalInfo: farmerObject.personalInfo,
+    familyInfo: farmerObject.familyInfo,
+    farmInfo: farmerObject.farmInfo,
+    guarantor: farmerObject.guarantor
   };
   const differences = await diff(originalFarmer, edit.edited_farmer);
 
-  const filteredDifferences = differences.filter(edit => {
+  /* const filteredDifferences = differences.filter(edit => {
     return !edit.path.includes('$init');
   });
-
+ */
   return {
-    changes: filteredDifferences.map(difference => {
+    changes: differences.map(difference => {
       if (difference.kind === 'D') {
         return {
           operation: 'deleted',
+          path: difference.path,
+          before: difference.lhs,
+          after: difference.rhs
+        };
+      }
+
+      if (difference.kind === 'A') {
+        return {
+          operation: difference.item.kind === 'N' ? 'added element' : 'removed element',
+          path: difference.path,
+          before: difference.item.lhs,
+          after: difference.item.rhs
+        };
+      }
+
+      if (difference.kind === 'N') {
+        return {
+          operation: 'created',
           path: difference.path,
           before: difference.lhs,
           after: difference.rhs
