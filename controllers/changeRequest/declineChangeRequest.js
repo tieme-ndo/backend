9eb637bb8de893c1315920b17f977cbc715ebac0
David Quiñones <daquinons@users.noreply.github.com>
2019-09-12 13:21:44 +0200

    Fix typo

--- controllers/changeRequest/declineChangeRequest.js
+++ controllers/changeRequest/declineChangeRequest.js
@@ -9,20 +9,22 @@ const { createError, GENERIC_ERROR } = require('../../helpers/error.js');
  * @param {object} next
  */
 
-const declinecCangeRequest = async (req, res, next) => {
+const declineChangeRequest = async (req, res, next) => {
   try {
-    const changeRequestEntry = await models.ChangeRequest.findOneAndRemove({ _id: req.params.id });
+    const changeRequestEntry = await models.ChangeRequest.findOneAndRemove({
+      _id: req.params.id
+    });
     if (changeRequestEntry) {
-        return res.status(200).json({
-          success: true,
-          message: 'ChangeRequest declined',
-        });
+      return res.status(200).json({
+        success: true,
+        message: 'ChangeRequest declined'
+      });
     }
     return res.status(404).json({
-        success: false,
-        message: 'There is no saved changeRequest with this ID, please subit a valid changeRequest-ID'
-    })
-
+      success: false,
+      message:
+        'There is no saved changeRequest with this ID, please subit a valid changeRequest-ID'
+    });
   } catch (err) {
     return next(
       createError({
