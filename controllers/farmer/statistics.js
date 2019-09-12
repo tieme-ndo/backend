e115471dfe287e55b8cd10ea8a0fd9a0b44fc18a
abidex4yemi <abidex4yemi@gmail.com>
2019-09-12 16:25:03 +0100

    Complete get farmers statistic feature

--- controllers/farmer/statistics.js
+++ controllers/farmer/statistics.js
@@ -13,35 +13,32 @@ const statistics = async (req, res, next) => {
   try {
     const allFarmers = await models.Farmer.find({});
 
-    // const totalNumOfFarmers = allFarmers.length;
+    const totalNumOfFarmers = allFarmers.length;
 
-    // const totalNumOfFemaleFarmers = allFarmers.filter(
-    //   (farmer) => farmer.personalInfo.gender.toLowerCase() === 'female'
-    // ).length;
+    const totalNumOfFemaleFarmers = allFarmers.filter(
+      (farmer) => farmer.personalInfo.gender.toLowerCase() === 'female'
+    ).length;
 
-    // const totalNumOfMaleFarmers = allFarmers.filter(
-    //   (farmer) => farmer.personalInfo.gender.toLowerCase() === 'male'
-    // ).length;
+    const totalNumOfMaleFarmers = allFarmers.filter(
+      (farmer) => farmer.personalInfo.gender.toLowerCase() === 'male'
+    ).length;
 
-    // const farmersAgeGreaterThanOrEqualThirtyFive = allFarmers.filter(
-    //   (farmer) => calculateAge(farmer.personalInfo.date_of_birth) >= 35
-    // ).length;
+    const farmersAgeGreaterThanOrEqualThirtyFive = allFarmers.filter(
+      (farmer) => calculateAge(farmer.personalInfo.date_of_birth) >= 35
+    ).length;
 
-    // const farmersAgeLesserThanThirtyFive = allFarmers.filter(
-    //   (farmer) => calculateAge(farmer.personalInfo.date_of_birth) < 35
-    // ).length;
+    const farmersAgeLesserThanThirtyFive = allFarmers.filter(
+      (farmer) => calculateAge(farmer.personalInfo.date_of_birth) < 35
+    ).length;
 
-    // const farmerStatisticsInNumbers = {
-    //   totalNumOfFarmers,
-    //   totalNumOfMaleFarmers,
-    //   totalNumOfFemaleFarmers,
-    //   farmersAgeGreaterThanOrEqualThirtyFive,
-    //   farmersAgeLesserThanThirtyFive
-    // };
-    console.log(`try block${allFarmers}`);
-    return res.status(200).json(allFarmers);
+    return res.status(200).json({
+      totalNumOfFarmers,
+      totalNumOfMaleFarmers,
+      totalNumOfFemaleFarmers,
+      farmersAgeGreaterThanOrEqualThirtyFive,
+      farmersAgeLesserThanThirtyFive
+    });
   } catch (error) {
-    console.log(`catch block${error}`);
     return next(
       createError({
         message: 'Could not get farmers statistics',
