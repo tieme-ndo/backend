9f5bacb56bfc69d8c889bfc2355a0c32f695117c
abidex4yemi <abidex4yemi@gmail.com>
2019-09-12 16:25:03 +0100

    Complete get farmers statistic feature

--- controllers/farmer/statistics.js
+++ controllers/farmer/statistics.js
@@ -13,33 +13,33 @@ const statistics = async (req, res, next) => {
   try {
     const allFarmers = await models.Farmer.find({});
 
-    const totalNumOfFarmers = allFarmers.length;
+    // const totalNumOfFarmers = allFarmers.length;
 
-    const totalNumOfFemaleFarmers = allFarmers.filter(
-      (farmer) => farmer.personalInfo.gender.toLowerCase() === 'female'
-    ).length;
+    // const totalNumOfFemaleFarmers = allFarmers.filter(
+    //   (farmer) => farmer.personalInfo.gender.toLowerCase() === 'female'
+    // ).length;
 
-    const totalNumOfMaleFarmers = allFarmers.filter(
-      (farmer) => farmer.personalInfo.gender.toLowerCase() === 'male'
-    ).length;
+    // const totalNumOfMaleFarmers = allFarmers.filter(
+    //   (farmer) => farmer.personalInfo.gender.toLowerCase() === 'male'
+    // ).length;
 
-    const farmersAgeGreaterThanOrEqualThirtyFive = allFarmers.filter(
-      (farmer) => calculateAge(farmer.personalInfo.date_of_birth) >= 35
-    ).length;
+    // const farmersAgeGreaterThanOrEqualThirtyFive = allFarmers.filter(
+    //   (farmer) => calculateAge(farmer.personalInfo.date_of_birth) >= 35
+    // ).length;
 
-    const farmersAgeLesserThanThirtyFive = allFarmers.filter(
-      (farmer) => calculateAge(farmer.personalInfo.date_of_birth) < 35
-    ).length;
+    // const farmersAgeLesserThanThirtyFive = allFarmers.filter(
+    //   (farmer) => calculateAge(farmer.personalInfo.date_of_birth) < 35
+    // ).length;
 
-    const farmerStatisticsInNumbers = {
-      totalNumOfFarmers,
-      totalNumOfMaleFarmers,
-      totalNumOfFemaleFarmers,
-      farmersAgeGreaterThanOrEqualThirtyFive,
-      farmersAgeLesserThanThirtyFive
-    };
+    // const farmerStatisticsInNumbers = {
+    //   totalNumOfFarmers,
+    //   totalNumOfMaleFarmers,
+    //   totalNumOfFemaleFarmers,
+    //   farmersAgeGreaterThanOrEqualThirtyFive,
+    //   farmersAgeLesserThanThirtyFive
+    // };
 
-    return res.status(200).json(farmerStatisticsInNumbers);
+    return res.status(200).json(allFarmers);
   } catch (error) {
     return next(
       createError({
