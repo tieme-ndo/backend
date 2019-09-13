298a802ce151858aedf544c66e10a8b7ffb8b4ca
quansenB <inaki.breinbauer@gmail.com>
2019-09-13 14:38:09 +0200

    clean up data and build response object, test endpoint

--- controllers/changeRequest/getChangeRequests.js
+++ controllers/changeRequest/getChangeRequests.js
@@ -45,7 +45,7 @@ const getChangeRequestById = async (req, res, next) => {
   try {
     const changeRequest = await models.ChangeRequest.findOne({
       _id: req.params.id
-    });
+    }).lean();
 
     if (!changeRequest) {
       return res.status(404).json({
@@ -54,21 +54,36 @@ const getChangeRequestById = async (req, res, next) => {
       });
     }
 
-    
-    const requestedChanges = changeRequest.requested_changes;
-    const selectFilter= convertToDotNotationObject(requestedChanges);
-    console.log(selectFilter);
-    const toChangeFarmer = await models.Farmer.findOne({
-      _id: changeRequest.farmer_id
-    }, selectFilter).lean();
-    console.log(toChangeFarmer);
+    /*  This block of code is used to query the DB with a filter derived from the changeRequest
+        to only display the data we need.
+        Mongoose includes the _id anyway, so it is deleted afterwards. 
+    */
+    const requested_changes = changeRequest.requested_changes;
+    const selectFilter = Object.keys(
+      convertToDotNotationObject(requested_changes)
+    );
+    const original_data = await models.Farmer.findOne(
+      {
+        _id: changeRequest.farmer_id
+      },
+      selectFilter
+    ).lean();
+    delete original_data._id;
+
+    const responseObject = {
+      requested_changes,
+      original_data,
+      id: changeRequest._id,
+      farmer_name: changeRequest.farmer_name,
+      change_requested_by: changeRequest.change_requested_by,
+      datetime: changeRequest.date
+    };
 
     return res.status(200).json({
       success: true,
       message: 'ChangeRequest with this ID found',
-      requestedChanges
+      ...responseObject
     });
-
   } catch (err) {
     return next(
       createError({
