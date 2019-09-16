ee7346d2e58b7f10166fdcb57a4a601e30701ef5
David Quiñones <daquinons@users.noreply.github.com>
2019-09-16 10:13:25 +0200

    Fix mistakes

--- controllers/changeRequest/approveChangeRequest.js
+++ controllers/changeRequest/approveChangeRequest.js
@@ -1,5 +1,6 @@
-const { models } = require("../../models");
-const { createError, GENERIC_ERROR } = require("../../helpers/error.js");
+const { models } = require('../../models');
+const { createError, GENERIC_ERROR } = require('../../helpers/error.js');
+const convertToDotNotationObject = require('../farmer/convertToDotNotationObject');
 
 /**
  * @description Get VhangeRequests
@@ -11,30 +12,34 @@ const { createError, GENERIC_ERROR } = require("../../helpers/error.js");
 
 const approveChangeRequest = async (req, res, next) => {
   try {
-    const changeRequestEntry = await models.ChangeRequest.findOne({ _id: req.params.id });
+    const changeRequestEntry = await models.ChangeRequest.findOne({
+      _id: req.params.id
+    });
     if (changeRequestEntry) {
-      const convertedObject = convertToDotNotationObject(changeRequestEntry);
-        await models.Farmer.findOneAndUpdate(
-          { _id: farmerId },
-          convertedObject,
-          { new: true, runValidators: true }
-        )
+      const convertedObject = convertToDotNotationObject(
+        changeRequestEntry.requested_changes
+      );
+      await models.Farmer.findOneAndUpdate(
+        { _id: changeRequestEntry.farmer_id },
+        convertedObject,
+        { new: true, runValidators: true }
+      );
       await models.ChangeRequest.findOneAndRemove({ _id: req.params.id });
       return res.status(200).json({
         success: true,
-        message: "ChangeRequest approved",
+        message: 'ChangeRequest approved',
         editedFarmer
       });
     }
     return res.status(404).json({
       success: false,
       message:
-        "There is no saved changeRequest with this ID, please subit a valid changeRequest-ID"
+        'There is no saved changeRequest with this ID, please subit a valid changeRequest-ID'
     });
   } catch (err) {
     return next(
       createError({
-        message: "Internal database error",
+        message: 'Internal database error',
         status: GENERIC_ERROR
       })
     );
