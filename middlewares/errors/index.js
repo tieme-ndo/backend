114aa584fc88a28e08c7115f1c376b6dfd8cddc2
Rexben <rexben.rb@gmail.com>
2019-09-19 13:18:12 +0100

    fix eslint warnings

--- middlewares/errors/index.js
+++ middlewares/errors/index.js
@@ -1,5 +1,6 @@
 const badRequest = require('./badRequest');
 const notFound = require('./notFound');
+const resourceConflict = require('./resourceConflict');
 const forbidden = require('./forbidden');
 const unauthorized = require('./unauthorized');
 const genericError = require('./genericError');
@@ -10,6 +11,7 @@ const genericError = require('./genericError');
 const errorsObject = {
   badRequest,
   notFound,
+  resourceConflict,
   forbidden,
   unauthorized,
   genericError,
